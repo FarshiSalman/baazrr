502dacad21d0c6cbddcd82415a23d39db0f8495d
Salman Farshi <37033900+FarshiSalman@users.noreply.github.com>
2023-02-05 17:27:46 +0600

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+////////////////////////////
+///CONFIG FILE FOR TAILWIND
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
+  plugins: [require('tw-elements/dist/plugin')],
+};
